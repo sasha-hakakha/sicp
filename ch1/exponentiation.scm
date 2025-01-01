7dc9110f43b7c9f7bfa098b31872dba9ca4d3141
Sasha Hakakha <sashahakakha@gmail.com>
2025-01-01 15:08:20 -0800

    this book was written by a psycho

---
+++ ch1/exponentiation.scm
@@ -0,0 +1,17 @@
+(use-modules (general utils))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (exp a b n)
+  (cond ((= n 0) a)
+	((even? n) (exp a (* b b) (/ n 2)))
+	(else (exp (* a b) b (- n 1)))))
+
+(define (exp-iter b n)
+  (exp 1 b n))
+
+(println (exp-iter 6 9))
+
+	
+	
